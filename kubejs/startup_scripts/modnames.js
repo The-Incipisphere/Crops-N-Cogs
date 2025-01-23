a2300809a05c2cc065a0f66be0e15ccce5621d73
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-01-23 23:25:08 +0000

    This is going to be heavily cursed, but one must do what one must do.
    Specifically, I'm adding PNC:R and Mekanism, but to gut their functionality out almost as much as i'm gonna gut out Gregtech.
    
    i just want the capabilities for multiblocked2
    I just want the capabilities.
    I JUST WANT THE CAPABILITES.

---
+++ kubejs/startup_scripts/modnames.js
@@ -0,0 +1,4 @@
+Platform.mods.kubejs.name = "KubeJS (my beloved)"
+Platform.mods.gtceu.name = "GTCEuM (Framework'd)"
+Platform.mods.mekanism.name = "Mekanism (Framework'd)" 
+Platform.mods.pneumaticcraft.name = "Pneumaticraft (Framework'd)"

085be9e59b4a9a6b15a991791310a34af6bae71b
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-01-26 01:47:02 +0000

    custom block for the cobblestone furnace's shape, new textures for the cobblestone furnace itself

---
+++ kubejs/startup_scripts/registry/blocks.js
@@ -0,0 +1,10 @@
+StartupEvents.registry('block', event => {
+    event.create('packed_cobblestone')
+        .displayName('Packed Cobblestone')
+        .stoneSoundType()
+        .hardness(5)
+        .resistance(15)
+        .requiresTool(true)
+        .tagBlock('minecraft:mineable/pickaxe')
+        .tagBlock('minecraft:needs_iron_tool')
+})

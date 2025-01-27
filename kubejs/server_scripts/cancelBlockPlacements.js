7c4998608c13396908c69f0349ad4358409de715
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-01-27 16:39:14 +0000

    ok, machine sounds are fucked, and there is seemingly no way for me to force-drop the controller without loading java classes...

--- kubejs/server_scripts/cancelBlockPlacements.js
+++ kubejs/server_scripts/cancelBlockPlacements.js
@@ -1,6 +1,7 @@
 global.multiblockControllerBlocks.forEach(element => { // Cancel block placement events if the block is a controller block.
     BlockEvents.placed(element, event => {
         event.player.displayClientMessage("Multiblock Controllers are not meant to be placed by players!", true);
+        // TODO: force the player to drop the block upon placing, as cancelling the block placement event this way causes it to be a 'Ghost item'
         event.cancel();
     }); 
 });

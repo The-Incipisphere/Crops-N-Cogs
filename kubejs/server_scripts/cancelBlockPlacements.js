e15fd2655e50704eae773b62e95d954a95e621f6
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-01-27 23:11:34 +0000

    make full utilisation of ImmersiveMessages, i must

--- kubejs/server_scripts/cancelBlockPlacements.js
+++ kubejs/server_scripts/cancelBlockPlacements.js
@@ -1,7 +1,6 @@
 global.multiblockControllerBlocks.forEach(element => { // Cancel block placement events if the block is a controller block.
     BlockEvents.placed(element, event => {
-        event.player.displayClientMessage("Multiblock Controllers are not meant to be placed by players!", true);
-        // TODO: force the player to drop the block upon placing, as cancelling the block placement event this way causes it to be a 'Ghost item'
+        event.server.runCommand("/immersivemessages sendcustom "+event.player.username+" {y:150} 3 Multiblock Controllers are not meant to be placed by players!")
         event.cancel();
     }); 
 });
